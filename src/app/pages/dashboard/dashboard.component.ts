173cbf4772bf604ff899cf17129015f3c7ea316e
Muskan <inamdarmuskan55@gmail.com>
2023-02-10 15:19:38 +0530

    Export to excel
    Export to excel

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -5,19 +5,26 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import {MatTableDataSource} from '@angular/material/table';
 import { Courses } from 'src/app/models/courses.model';
 import { CoursesService } from 'src/app/services/courses.service';
-
+import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit{
+  filename="automation.xlsx"
   constructor(
     private _courses:CoursesService,
     private _snackBar: MatSnackBar, 
 
   ){ }
-
+  exportexcel():void{
+    let element=document.getElementById('exceltable')
+    const ws:XLSX.WorkSheet=XLSX.utils.table_to_sheet(element,{raw:true})
+    const wb:XLSX.WorkBook=XLSX.utils.book_new()
+    XLSX.utils.book_append_sheet(wb,ws,'Sheet1')
+    XLSX.writeFile(wb,this.filename)
+  }
   allCourses: Courses[] = [];
   dataSource: any[] = [];
   educator= '';
@@ -34,7 +41,6 @@ export class DashboardComponent implements OnInit{
     
     
   }
-
   getAllCourses(){
     this._courses.getAllCourses().subscribe(
       (res)=>{
@@ -64,16 +70,6 @@ export class DashboardComponent implements OnInit{
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
 
 export interface PeriodicElement {
   name: string;
