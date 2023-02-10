b1d2676edc08e78cfa02bdfff0e6a92c35e3ff40
AeromusGyan <73412017+AeromusGyan@users.noreply.github.com>
2023-02-10 11:25:54 +0530

    sorting is done

--- src/app/pipes/courses-sort.pipe.ts
+++ src/app/pipes/courses-sort.pipe.ts
@@ -5,28 +5,36 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CoursesSortPipe implements PipeTransform {
 
-  transform(value: any[], args?: any): any[]  {
-    if (args === 'prodName') {
-      return value.sort((a: any, b: any) => {
-        if (a.productName < b.productName) {
-          return -1;
-        } else if (a.productName > b.productName) {
-          return 1;
-        } else {
-          return 0;
-        }
-      });
-    } else if (args === 'price') {
-      return value.sort((a: any, b: any) => {
-        if (a.price < b.price) {
-          return -1;
-        } else if (a.price > b.price) {
-          return 1;
-        } else {
-          return 0;
-        }
-      });
+  transform(value: any[], args?: any): any[] {
+    console.log(value, args);
+    for (let index = 0; index < value.length; index++) {
+    const data:any[] = [];
+      if (args === value[index].course_name) {
+        data.push(value[index]);
+        return data;
+      }
+      else if (args === value[index].educator.educator_name) {
+        data.push(value[index]);
+        return data;
+      }
+      else if (args === value[index].course_mode) {
+        data.push(value[index]);
+        return data;
+      }
+      else if (args === value[index].start_date) {
+        data.push(value[index]);
+        return data;
+      }
+      else if (args === value[index].end_date) {
+        data.push(value[index]);
+        return data;
+      }
+      else if (args === value[index].contact_session_timing) {
+        data.push(value[index]);
+        return data;
+      }
     }
+    // 
     return value;
   }
 
