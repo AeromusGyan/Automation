0d6130091d8a2f9b17b6f9d3acfe8ef55c087f27
AeromusGyan <73412017+AeromusGyan@users.noreply.github.com>
2023-03-22 00:46:12 +0530

    updATE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginService } from './services/login.service';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Automation';
-  constructor(){
+  
+  userdata!:any;
+  logout(){
+    this.api.logout();
+    window.location.href = "/login"
+  }
+  constructor(private api:LoginService){
+    // console.log(this.userdata);
+    
+    this.userdata = this.api.getUser();
     if(navigator.onLine) {
       console.log("You are Online")
      }
